66f2371e0bb78cd4164a591fe535a0c959a1f1e2
craig <lukashroch@hotmail.com>
2017-07-29 09:24:29 +0200

    * update cache model flushing to support both Taggable store and no tags

--- app/FlushableTrait.php
+++ app/FlushableTrait.php
@@ -2,20 +2,46 @@
 
 namespace ChemLab;
 
+use Illuminate\Cache\TaggableStore;
+
 trait FlushableTrait
 {
+    /**
+     * Boot Trait
+     *
+     * @return void
+     */
     public static function bootFlushableTrait()
     {
         static::saved(function ($model) {
-            //Cache::tags(strtolower(class_basename($model)))->flush();
+            $model->flushModelCache();
+        });
+
+        static::deleted(function ($model) {
+            $model->flushModelCache();
+        });
+    }
+
+    /**
+     * Flush Model cache data
+     *
+     * @return void
+     */
+    public function flushModelCache()
+    {
+        $cache = cache();
+        $class = strtolower(class_basename($this));
+
+        if ($cache->getStore() instanceof TaggableStore) {
+            $cache->tags($class)->flush();
+        } else {
             if (!isset(static::$cacheKeys) || empty(static::$cacheKeys))
                 return;
 
-            $class = strtolower(class_basename($model));
             foreach (static::$cacheKeys as $cacheKey) {
-                if (cache()->has($class . '-' . $cacheKey))
-                    cache()->forget($class . '-' . $cacheKey);
+                if ($cache->has($class . '-' . $cacheKey))
+                    $cache->forget($class . '-' . $cacheKey);
             }
-        });
+        }
     }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Cache\TaggableStore;
+
 function settings($key = null)
 {
     $settings = app('ChemLab\Settings');
@@ -9,4 +11,15 @@ function settings($key = null)
 function path($path, $local = false)
 {
     return $local ? $path : storage_path() . '/app/' . $path;
+}
+
+function localCache($prefix, &$key)
+{
+    $cache = cache();
+    if ($cache->getStore() instanceof TaggableStore) {
+        return $cache->tags($prefix);
+    } else {
+        $key = $prefix . "-" . $key;
+        return $cache;
+    }
 }
